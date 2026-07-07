82639f56988445ebd21150ee62d1593ed4a19914
Manan Singh <38788587+MananSingh2001@users.noreply.github.com>
2026-07-07 22:10:58 +0530

    Implement first unique character finder

---
+++ hashMap/First Unique Character in a String.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char,int> mpp;
+        int n=s.size();
+
+        for(int i=0;i<n;i++)
+        {
+            mpp[s[i]]++;
+        }
+
+        for( int i=0;i<n;i++)
+        {
+            if (mpp[s[i]] == 1)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
